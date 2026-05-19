110e129adb8bdcd99f5d3f9516df4d7a7ae48f2b
EliteFeeLInG <kevinpomian@icloud.com>
2026-05-19 11:27:19 +0200

    Fix: css

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const myImgs2 = [
-    { path: "alexanderweichsel-japan-9074037_1280.jpg", desc: "Foto von Torii-Tor", klasse: "arrayclass" },
-    { path: "binmassam-chef-4807317_1280.jpg", desc: "Foto von Streetfood Japan", klasse: "arrayclass" },
-    { path: "deltaworks-japanese-umbrellas-636870_1280.jpg", desc: "Foto von Regenschirme", klasse: "arrayclass" },
-    { path: "djedj-japanese-lantern-5568727_1280.jpg", desc: "Foto von einer Dekoration", klasse: "arrayclass" },
-    { path: "jplenio-traditional-8245250_1280.jpg", desc: "Foto von einem Raum", klasse: "arrayclass" },
-    { path: "kanenori-mountains-8031511_1280.jpg", desc: "Foto auf einem Berg in der nacht", klasse: "arrayclass" },
-    { path: "kanenori-temple-6963458_1280.jpg", desc: "Foto von einem Tempel in der nacht", klasse: "arrayclass" },
-    { path: "mailtotobi-waterfall-4259935_1280.jpg", desc: "Foto von einem Wasserfall", klasse: "arrayclass" },
-    { path: "maximilianhemon-japan-5021733_1280.jpg", desc: "Foto vom Nachtleben", klasse: "arrayclass" },
-    { path: "pexels-temple-1841296_1280.jpg", desc: "Foto von einem Tempel am Tag", klasse: "arrayclass" },
-    { path: "rmsep4-street-4942809_1280.jpg", desc: "Foto von straße in der Nacht", klasse: "arrayclass" },
-    { path: "takatoshikun-cafe-5899078_1280.jpg", desc: "Foto von einem kleinen Laden außen", klasse: "arrayclass" }
+    { path: "alexanderweichsel-japan-9074037_1280.jpg", desc: "Foto von Torii-Tor", class: "arrayclass" },
+    { path: "binmassam-chef-4807317_1280.jpg", desc: "Foto von Streetfood Japan", class: "arrayclass" },
+    { path: "deltaworks-japanese-umbrellas-636870_1280.jpg", desc: "Foto von Regenschirme", class: "arrayclass" },
+    { path: "djedj-japanese-lantern-5568727_1280.jpg", desc: "Foto von einer Dekoration", class: "arrayclass" },
+    { path: "jplenio-traditional-8245250_1280.jpg", desc: "Foto von einem Raum", class: "arrayclass" },
+    { path: "kanenori-mountains-8031511_1280.jpg", desc: "Foto auf einem Berg in der nacht", class: "arrayclass" },
+    { path: "kanenori-temple-6963458_1280.jpg", desc: "Foto von einem Tempel in der nacht", class: "arrayclass" },
+    { path: "mailtotobi-waterfall-4259935_1280.jpg", desc: "Foto von einem Wasserfall", class: "arrayclass" },
+    { path: "maximilianhemon-japan-5021733_1280.jpg", desc: "Foto vom Nachtleben", class: "arrayclass" },
+    { path: "pexels-temple-1841296_1280.jpg", desc: "Foto von einem Tempel am Tag", class: "arrayclass" },
+    { path: "rmsep4-street-4942809_1280.jpg", desc: "Foto von straße in der Nacht", class: "arrayclass" },
+    { path: "takatoshikun-cafe-5899078_1280.jpg", desc: "Foto von einem kleinen Laden außen", class: "arrayclass" }
 ]
 
 
@@ -21,7 +21,7 @@ function renderImgs() {
     let images = "";
     for (Image in myImgs2) {
         images += `
-        <button onclick="openDialog(${Image})"class=" ${myImgs2[Image].klasse} " ${myImgs2[Image].activ}>
+        <button onclick="openDialog(${Image})"class=" ${myImgs2[Image].class} ">
         <img src="assats/img/${myImgs2[Image].path}" alt="${myImgs2[Image].desc}" id="image${Image}">
         </button>
         `
@@ -39,12 +39,13 @@ function renderDialog() {
     dialog += `
         <div class="dialogHeader">
             <h2>Bild: ${myImgs2[activeImg].desc}</h2>
-        <button id="closeButton" onclick="closeDialog()">X</button>
+            <button id="closeButton" onclick="closeDialog()">X</button>
         </div>
 
         <div class="dialogImg">
-        <img src="./assats/img/${myImgs2[activeImg].path}" alt="${myImgs2[activeImg].desc}" >
+            <img src="./assats/img/${myImgs2[activeImg].path}" alt="${myImgs2[activeImg].desc}" >
         </div>
+
         <nav>
             <button onclick="dialogBack()" id="backButton">←</button>
             <p class="counter">${activeImg + 1}/${myImgs2.length}</p>
